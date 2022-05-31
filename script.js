018973f48bf7f8419fac60051665a1add082e5c5
kenjiclayton <52931772+kenjiclayton@users.noreply.github.com>
2022-06-01 00:39:07 +0800

    New Update
    Skill Sample Referrences

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 
    /* typing animation script */
     var typed = new Typed(".typing", {
-       strings: ["Developer", "GameDeveloper", "Logo Designer", "2D Pixel Artist"],
+       strings: ["GameDeveloper", "Graphics Designer", "2D Pixel Artist"],
        typeSpeed: 100,
        backSpeed: 60,
        loop: true
    });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "GameDeveloper", "Logo Designer", "2D Pixel Artist"],
+        strings: ["GameDeveloper", "Graphics Designer", "2D Pixel Artist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
