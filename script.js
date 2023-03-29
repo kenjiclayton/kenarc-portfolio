fd0f3a0d5dcd65908fae84b1e54b00b759842f4d
Kenneth Cayton <kenjiclayton@yahoo.com>
2023-03-29 11:40:05 +0800

    Massive Update
    html css js updates

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 
    /* typing animation script */
     var typed = new Typed(".typing", {
-       strings: ["Developer", "Graphics Designer", "2D Pixel Artist"],
+       strings: ["Developer", "Graphics Artist", "Machinist"],
        typeSpeed: 100,
        backSpeed: 60,
        loop: true
    });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Graphics Designer", "2D Pixel Artist"],
+        strings: ["Developer", "Graphics Artist", "Machinist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
