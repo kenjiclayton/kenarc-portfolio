6914c99fd43ac3ca4a913def1b9ede78339999a9
kenjiclayton <52931772+kenjiclayton@users.noreply.github.com>
2024-08-27 18:10:45 +0800

    update
    remove and fixing some images

--- script.js
+++ script.js
@@ -25,14 +25,14 @@ $(document).ready(function(){
 
    /* typing animation script */
     var typed = new Typed(".typing", {
-       strings: ["Developer", "Graphics Artist", "Machinist"],
+       strings: ["Graphics Designer", "Graphics Artist"],
        typeSpeed: 100,
        backSpeed: 60,
        loop: true
    });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Developer", "Graphics Artist", "Machinist"],
+        strings: ["Graphics Designer", "Graphics Artist"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
